803b69eb5f694c338a67f7b5602703cb77a7d7ba
narequipa <narequipav@est.ups.edu.ec>
2017-06-18 00:06:53 -0500

    tesis
    jbbhjbjhbhj
    ded

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="es">
+
+    <head>
+
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <title>Byphone - Xerox</title>
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/NavBarSide.css" rel="stylesheet">
+        <link href="css/style.css" rel="stylesheet">
+    </head>
+
+    <body>
+        <div id="wrapper">
+            <div class="overlay"></div>
+            <nav class="navbar navbar-inverse navbar-fixed-top" id="sidebar-wrapper" role="navigation">
+                <ul class="nav sidebar-nav">
+                    <li class="sidebar-brand">
+                        <a href="index.php">
+                            BYPHONE
+                        </a>
+                    </li>
+                    <li>
+                        <a href="home.html">Home</a>
+                    </li>
+                    <li>
+                        <a href="record.html">Quejas</a>
+                    </li>
+
+                   
+
+                </ul>
+            </nav>
+
+            <!-- /#sidebar-wrapper -->
+
+            <!-- Page Content -->
+            <div id="page-content-wrapper">
+                <center>
+                    <img src="images/logo.jpg" class="img-responsive" alt="Responsive image">
+                <button type="button" class="hamburger is-closed" data-toggle="offcanvas">
+                    <span class="hamb-top"></span>
+                    <span class="hamb-middle"></span>
+                    <span class="hamb-bottom"></span>
+                </button>
+                </center>
+
+            </div>
+            <!-- /#page-content-wrapper -->
+
+        </div>
+        <!-- /#wrapper -->
+
+        <script src="js/jquery.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/NavBarSide.js"></script>
+    </body>
+</html>
